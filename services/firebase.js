0bb0d6ccd772bba6d4a382e50aa27220db5e02f1
maccomxkml <kmel912@gmail.com>
2024-10-06 22:55:02 +0200

    .

--- services/firebase.js
+++ services/firebase.js
@@ -5,12 +5,12 @@ import { getStorage} from 'firebase/storage';
 
 // Configuration Firebase pour ton projet
 const firebaseConfig = {
-  apiKey: "AIzaSyDRY-j70w6lKRj0_9DGImi_Wr1Di-ZLCHg",
-  authDomain: "camerastock-c5827.firebaseapp.com",
-  projectId: "camerastock-c5827",
-  storageBucket: "camerastock-c5827.appspot.com",
-  messagingSenderId: "397659761555",
-  appId: "1:397659761555:web:78c9aedf50640e52999666"
+  apiKey: "AIzaSyABI-GvNcR32yUAS4-BWlYka4BBv6rTRMw",
+  authDomain: "illuminations-vda.firebaseapp.com",
+  projectId: "illuminations-vda",
+  storageBucket: "illuminations-vda.appspot.com",
+  messagingSenderId: "574292872540",
+  appId: "1:574292872540:web:8cbbad8daa51c02b470599"
 };
 
 // Initialiser l'application Firebase
